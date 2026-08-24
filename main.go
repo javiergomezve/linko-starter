3938b3e43d0cb77e8599e9c80ceafd826e3e8de7
Javier Gomez <javiergomezve@gmail.com>
2026-08-23 21:09:34 -0500

    Creating a centralized logger

--- main.go
+++ main.go
@@ -15,40 +15,42 @@ import (
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 
+	logger := log.New(os.Stderr, "DEBUG: ", log.LstdFlags)
+
 	httpPort := flag.Int("port", 8899, "port to listen on")
 	dataDir := flag.String("data", "./data", "directory to store data")
 	flag.Parse()
 
-	status := run(ctx, cancel, *httpPort, *dataDir)
+	status := run(ctx, cancel, *httpPort, *dataDir, logger)
 	cancel()
 
-	log.Print("Linko is shutting down")
+	logger.Print("Linko is shutting down")
 	os.Exit(status)
 }
 
-func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string) int {
-	st, err := store.New(dataDir)
+func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string, logger *log.Logger) int {
+	st, err := store.New(dataDir, logger)
 	if err != nil {
-		log.Printf("failed to create store: %v\n", err)
+		logger.Printf("failed to create store: %v\n", err)
 		return 1
 	}
-	s := newServer(*st, httpPort, cancel)
+	s := newServer(*st, httpPort, cancel, logger)
 	var serverErr error
 	go func() {
 		serverErr = s.start()
 	}()
 
-	log.Printf("Linko is running on http://localhost:%d", httpPort)
+	logger.Printf("Linko is running on http://localhost:%d", httpPort)
 	<-ctx.Done()
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	if err := s.shutdown(shutdownCtx); err != nil {
-		log.Printf("failed to shutdown server: %v\n", err)
+		logger.Printf("failed to shutdown server: %v\n", err)
 		return 1
 	}
 	if serverErr != nil {
-		log.Printf("server error: %v\n", serverErr)
+		logger.Printf("server error: %v\n", serverErr)
 		return 1
 	}
 	return 0
