e8dab4280b200df6e2fe6c16c65afe74d30808aa
Javier Gomez <javiergomezve@gmail.com>
2026-09-08 13:26:37 -0500

    Use the bootdev version

--- main.go
+++ main.go
@@ -118,27 +118,27 @@ type stackTracer interface {
 }
 
 func replaceAttr(groups []string, a slog.Attr) slog.Attr {
-	if a.Key != "error" {
-		return a
+	if a.Key == "error" {
+		err, ok := a.Value.Any().(error)
+		if !ok {
+			return a
+		}
+		attrs := []slog.Attr{
+			{
+				Key:   "message",
+				Value: slog.StringValue(err.Error()),
+			},
+		}
+
+		attrs = append(attrs, linkoerr.Attrs(err)...)
+
+		if stackErr, ok := errors.AsType[stackTracer](err); ok {
+			attrs = append(attrs, slog.Attr{
+				Key:   "stack_trace",
+				Value: slog.StringValue(fmt.Sprintf("%+v", stackErr.StackTrace())),
+			})
+		}
+		return slog.GroupAttrs("error", attrs...)
 	}
-
-	err, ok := a.Value.Any().(error)
-	if !ok {
-		return a
-	}
-
-	groupAttrs := []slog.Attr{
-		{Key: "message", Value: slog.StringValue(err.Error())},
-	}
-
-	if stackErr, ok := errors.AsType[stackTracer](err); ok {
-		groupAttrs = append(groupAttrs, slog.Attr{
-			Key:   "stack_trace",
-			Value: slog.StringValue(fmt.Sprintf("%+v", stackErr.StackTrace())),
-		})
-	}
-
-	groupAttrs = append(groupAttrs, linkoerr.Attrs(err)...)
-
-	return slog.GroupAttrs("error", groupAttrs...)
+	return a
 }
